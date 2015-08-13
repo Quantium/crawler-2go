529b57066fca628577a542d9cc5261cc279ab46d
Marco Godínez <markotom@gmail.com>
2015-08-13 18:56:14 -0500

    Updated crawler using xray module

--- crawler.js
+++ crawler.js
@@ -1,11 +1,84 @@
 'use strict';
-var Crawler = require('simplecrawler');
-var model_url = require('./model_url');
 
-var myCrawler = new Crawler('www.bebe2go.com', '/');
-myCrawler.on('fetchcomplete', function(queueItem, responseBuffer, response) {
-    model_url.create({url :  queueItem.url });
-    // Do something with the data in responseBuffer
-});
-myCrawler.maxDepth = 3;
-myCrawler.start();
+require('colors');
+
+var xray = require('x-ray');
+var x = xray();
+
+var Crawler = function (params) {
+  params = params || {};
+
+  this.collections = [];
+  this.products = [];
+  this.result = [];
+  this.url = params.url;
+  this.categories = params.categories;
+};
+
+Crawler.prototype.steal = function (callback) {
+  x(this.url, this.categories, [{
+    title: 'a',
+    url: 'a@href',
+  }])(function(err, collections) {
+    if (err) { return console.log(err.toString().red); }
+
+    this.collections = collections || [];
+    this._collect(0, callback);
+  }.bind(this));
+};
+
+Crawler.prototype._collect = function (index, callback) {
+  index = index || 0;
+  var collection = this.collections[index];
+
+  if (collection) {
+    return x(collection.url, '.shopping-item', [{
+      title: '.productTitle',
+      url: 'a.productTitle@href'
+    }])(function (err, products) {
+      if (err) { return console.log(err.toString().red); }
+
+      console.log('Retrieving collection:'.cyan, collection.title.yellow);
+      if (products && products.length) {
+        for (var i = 0; i < products.length; i++) {
+          products[i].collection = collection.title;
+          this.products.push(products[i]);
+        }
+      }
+      this._collect(index + 1, callback);
+    }.bind(this));
+  }
+
+  console.log('================================'.white);
+  console.log('================================'.white);
+  console.log('================================'.white);
+
+  this._products(0, callback);
+};
+
+Crawler.prototype._products = function (index, callback) {
+  index = index || 0;
+  var product = this.products[index];
+  if (product) {
+    return x(product.url, '.shopping-content', [{
+      title: 'h4',
+      price: 'p.price',
+      images: ['img@src'],
+      description: '.product-desc #tab1 h3',
+      short_description: '.product-desc #tab1 p'
+    }])(function (err, product) {
+      product = product[0];
+
+      if (err) { return console.log(err.toString().red); }
+      console.log('Retrieving product:'.cyan, product.title.yellow);
+      if (product) {
+        this.result.push(product);
+      }
+      this._products(index + 1, callback);
+    }.bind(this));
+  }
+
+  callback(this.result);
+};
+
+module.exports = Crawler;
